1ec87b0da2a375d530f5646d739b842018506f00
jgssanchez <jgs.sanchez94@gmail.com>
2024-06-20 21:18:52 -0300

    se eliminarion archivos innecesarios

--- src/redux/actions/productAction.js
+++
@@ -1,8 +0,0 @@
-import { createAsyncThunk } from "@reduxjs/toolkit";
-import clientAxios from "../../utils/clientAxios";
-
-
-export const getProducts = createAsyncThunk("getProducts", async () => {
-    const response = await clientAxios.get(`/products/`);
-    return response.data;
-});
