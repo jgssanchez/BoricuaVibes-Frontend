fa321cdce256db5031400b09dd59e67fa648b051
jgssanchez <jgs.sanchez94@gmail.com>
2024-06-20 21:50:11 -0300

    se agregaron rutas para detalle producto, dashboard, y confirmacion de compra

--- src/App.jsx
+++ src/App.jsx
@@ -7,33 +7,50 @@ import RootLayout from "./components/RootLayout/RootLayout";
 import NotFound from "./pages/NotFound.jsx";
 import LoginPage from "./pages/LoginPage.jsx";
 import RegisterPage from "./pages/RegisterPage.jsx";
-
-import {  useDispatch } from "react-redux";
-import { useEffect, useState } from "react";
-import { getUser } from "./redux/actions/userActions.js";
 import AdminPage from "./pages/AdminPage.jsx";
-import { getProducts } from "./redux/actions/productAction.js";
-
-
-
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { getUser } from "./redux/actions/userActions.js";
+import Users from "./components/AdminDashboard/Users/Users.jsx";
+import Products from "./components/AdminDashboard/Products/Products.jsx";
+import Orders from "./components/AdminDashboard/Orders/Orders.jsx";
+import { getAllProducts } from "./redux/actions/productActions.js";
+import { getUserCart } from "./redux/actions/cartActions.js";
+import { getUserOrders } from "./redux/actions/orderActions.js";
+import CheckoutPage from "./pages/CheckoutPage.jsx";
+import ProductDetailsPage from "./pages/ProductDetailsPage.jsx";
 
 const App = () => {
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
+  const { isAuthenticated, loading } = useSelector((state) => state.user);
 
-  useEffect(() =>{
-    dispatch(getUser())
-    dispatch(getProducts())
-  }, [])
-  
+  useEffect(() => {
+    dispatch(getUser());
+    dispatch(getAllProducts());
+    if (isAuthenticated && !loading) {
+      dispatch(getUserCart());
+      dispatch(getUserOrders())
+    }
+  }, [isAuthenticated, dispatch]);
+
   return (
     <BrowserRouter>
       <RootLayout>
         <Routes>
           <Route path="/" element={<HomePage />} />
-          <Route path="/login" element={<LoginPage />} />
           <Route path="*" element={<NotFound />} />
+          <Route path="/login" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
-          <Route path="/admin" element={<AdminPage />} />
+          <Route path="/checkout" element={<CheckoutPage />} />
+          <Route path="/product/:id" element={<ProductDetailsPage />} />
+
+          
+          <Route path="/admin/*" element={<AdminPage />}>
+            <Route path="users" element={<Users />} />
+            <Route path="products" element={<Products />} />
+            <Route path="orders" element={<Orders />} />
+            
+          </Route>
         </Routes>
       </RootLayout>
     </BrowserRouter>
