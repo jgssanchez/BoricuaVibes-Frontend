a5c5507aa3378e9d71a0c1f72d3ed9e947837cea
Julio Sanchez <jgs.sanchez94@gmail.com>
2024-06-27 01:06:32 -0300

    cambie deploy a render

--- src/utils/clientAxios.js
+++ src/utils/clientAxios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import Cookies from 'js-cookie';
 
 const clientAxios = axios.create({
-    baseURL: "https://boricua-vibes-backend.vercel.app",
+    baseURL: "https://boricuavibes-backend.onrender.com",
     headers: {
         'Content-Type': 'application/json',
     },
